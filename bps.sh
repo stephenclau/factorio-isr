b7109f42703a6df76f32ae08337d126fef73a6c8
slau <stephen.c.lau@gmail.com>
2025-11-29 22:04:11 -0800

    removed references to autogenerated GITHUB_TOKEN because it doesn't always work

--- bps.sh
+++ bps.sh
@@ -8,7 +8,7 @@ IMAGE_NAME="factorio-isr"
 VERSION="${1:-latest}"  # Use first argument or default to 'latest'
 
 # Read tokens from files (secure approach)
-GITHUB_TOKEN=$(cat ~/.secrets/GITHUB_TOKEN.txt)
+GHCR_TOKEN=$(cat ~/.secrets/GHCR_TOKEN.txt)
 DOCKERHUB_TOKEN=$(cat ~/.secrets/DOCKER_TOKEN.txt)
 
 echo "Building image version: $VERSION"
@@ -17,7 +17,7 @@ docker build -t ghcr.io/$GITHUB_USER/$IMAGE_NAME:$VERSION \
              .
 
 echo "Logging in to GHCR..."
-echo "$GITHUB_TOKEN" | docker login ghcr.io -u $GITHUB_USER --password-stdin
+echo "$GHCR_TOKEN" | docker login ghcr.io -u $GITHUB_USER --password-stdin
 
 echo "Logging in to Docker Hub..."
 echo "$DOCKERHUB_TOKEN" | docker login -u $DOCKERHUB_USER --password-stdin
