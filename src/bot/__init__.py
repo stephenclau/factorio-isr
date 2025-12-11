81c59392ee4b998e53777567c5ac30d4cd967fbb
Stephen Lau <1522116+stephenclau@users.noreply.github.com>
2025-12-11 12:07:52 -0800

    feat: create bot module __init__.py

---
+++ src/bot/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2025 Stephen Clau
+#
+# This file is part of Factorio ISR.
+#
+# Factorio ISR is dual-licensed:
+#
+# 1. GNU Affero General Public License v3.0 (AGPL-3.0)
+#    See LICENSE file for full terms
+#
+# 2. Commercial License
+#    For proprietary use without AGPL requirements
+#    Contact: licensing@laudiversified.com
+#
+# SPDX-License-Identifier: AGPL-3.0-only OR Commercial
+
+"""Discord bot module - refactored components for modular architecture."""
+
+from .user_context import UserContextManager
+from .rcon_monitor import RconMonitor
+from .event_handler import EventHandler
+from .helpers import PresenceManager
+
+__all__ = [
+    "UserContextManager",
+    "RconMonitor",
+    "EventHandler",
+    "PresenceManager",
+]
